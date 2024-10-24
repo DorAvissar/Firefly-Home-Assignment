c500a09945694d5bbad41cb9e363cd3edf521fb4
doravissar <doravissar2@gmail.com>
2024-10-24 17:58:23 +0300

    add screenshots

--- Terraform/Terraform/0- provider.tf
+++ Terraform/Terraform/0- provider.tf
@@ -1,11 +1,14 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws" # Specifies the AWS provider source from HashiCorp's registry
-      version = "~> 5.0"        # Specifies the AWS provider version (any 5.x version)
+      source  = "hashicorp/aws"
+      version = "~> 5.0"  # Ensures you get the latest 5.x releases
     }
   }
+
+  required_version = ">= 1.5.0"  # Make sure you're using the latest Terraform CLI version
 }
+
 
 provider "aws" {
   region = "us-east-1" # Specifies the AWS region where the resources will be created
--- Terraform/Terraform/6- IAM.tf
+++ Terraform/Terraform/6- IAM.tf
@@ -1,59 +1,64 @@
 # IAM Role for EC2 instance
+# This role allows the EC2 instance to assume permissions defined in the attached policies.
 resource "aws_iam_role" "ec2_role" {
   name = "firefly_ec2_role"
 
+  # The assume_role_policy specifies which service (EC2) is allowed to assume this role.
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
       {
         Effect = "Allow"
         Principal = {
-          Service = "ec2.amazonaws.com"
+          Service = "ec2.amazonaws.com"  # EC2 service can assume this role
         }
-        Action = "sts:AssumeRole"
+        Action = "sts:AssumeRole"  # Permission to assume the role using STS
       }
     ]
   })
 }
 
-# IAM Policy to Allow Access to S3
+# This policy grants permissions for the EC2 instance to interact with the S3 bucket.
 resource "aws_iam_policy" "s3_access_policy" {
-  name        = "firefly_s3_policy"
-  description = "IAM policy for EC2 access to S3 bucket"
+  name        = "firefly_s3_policy"  # Name of the IAM policy
+  description = "IAM policy for EC2 access to S3 bucket"  # Brief description
 
+  # JSON-encoded policy specifying the allowed actions and resources for S3 access.
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
       {
-        Effect = "Allow"
+        Effect = "Allow"  # Grant access permissions
         Action = [
-          "s3:ListBucket",
-          "s3:GetObject",
-          "s3:PutObject"
+          "s3:ListBucket",  # List bucket contents
+          "s3:GetObject",    # Retrieve objects from the bucket
+          "s3:PutObject"     # Upload objects to the bucket
         ]
         Resource = [
-          "arn:aws:s3:::firefly-bucket-${random_string.suffix.result}",
-          "arn:aws:s3:::firefly-bucket-${random_string.suffix.result}/*"
+          "arn:aws:s3:::firefly-bucket-${random_string.suffix.result}",      # Access to the bucket itself
+          "arn:aws:s3:::firefly-bucket-${random_string.suffix.result}/*"     # Access to all objects within the bucket
         ]
       }
     ]
   })
 }
 
-# Attach the policy to the role
+
+# This resource attaches the S3 access policy to the EC2 IAM role.
 resource "aws_iam_role_policy_attachment" "s3_policy_attachment" {
-  role       = aws_iam_role.ec2_role.name
-  policy_arn = aws_iam_policy.s3_access_policy.arn
+  role       = aws_iam_role.ec2_role.name  # Role to which the policy is attached
+  policy_arn = aws_iam_policy.s3_access_policy.arn  # ARN of the S3 access policy
 }
 
 
-# Instance Profile to associate the IAM role with EC2
+# The instance profile allows the EC2 instance to assume the IAM role.
 resource "aws_iam_instance_profile" "ec2_profile" {
-  name = "firefly_ec2_profile"
-  role = aws_iam_role.ec2_role.name
+  name = "firefly_ec2_profile"  # Name of the instance profile
+  role = aws_iam_role.ec2_role.name  # IAM role associated with the instance profile
 }
 
+# This policy allows the EC2 instance to communicate with Systems Manager.
 resource "aws_iam_role_policy_attachment" "ssm_ec2" {
-  role       = aws_iam_role.ec2_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+  role       = aws_iam_role.ec2_role.name 
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"  
 }
